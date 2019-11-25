14388a98b2cbdbd7c0df60c30e7d4c2213994f82
Tobias Eiberle <utoppo@gmail.com>
2019-11-25 14:22:56 +0100

    debug message

--- command.php
+++ command.php
@@ -1,6 +1,8 @@
 <?php
 
 if (defined('WP_CLI') && class_exists('WP_CLI', false)) {
+    WP_CLI::line();
+    WP_CLI::line('\n\n\n==== INIT DOT ENV COMMAND');
     WP_CLI::add_command('dotenv', WP_CLI_Dotenv\WP_CLI\DotenvCommand::class);
     WP_CLI::add_command('dotenv salts', WP_CLI_Dotenv\WP_CLI\SaltsCommand::class);
 }
